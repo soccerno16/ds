c7b16688d2ccb3dbe2c967d30ef5bb8924dd157d
Ben <ben.payment@gmail.com>
2021-11-03 17:01:22 -0500

    Initial Commit

---
+++ pca10040/s132/ses/Output/Release/Obj/ble_app_beacon_pca10040_s132/app_error.d
@@ -0,0 +1,24 @@
+Output/Release/Obj/ble_app_beacon_pca10040_s132/app_error.o: \
+ C:\nordic\nRF5_SDK_17.1.0_ddde560\components\libraries\util\app_error.c \
+ ../../../../../../modules/nrfx/mdk/nrf.h \
+ ../../../../../../modules/nrfx/mdk/nrf52805.h \
+ ../../../../../../components/toolchain/cmsis/include/core_cm4.h \
+ C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 5.64/include/stdint.h \
+ ../../../../../../components/toolchain/cmsis/include/cmsis_version.h \
+ ../../../../../../components/toolchain/cmsis/include/cmsis_compiler.h \
+ ../../../../../../components/toolchain/cmsis/include/cmsis_gcc.h \
+ ../../../../../../components/toolchain/cmsis/include/mpu_armv7.h \
+ ../../../../../../modules/nrfx/mdk/system_nrf52805.h \
+ ../../../../../../modules/nrfx/mdk/nrf52805_bitfields.h \
+ ../../../../../../modules/nrfx/mdk/nrf51_to_nrf52810.h \
+ ../../../../../../modules/nrfx/mdk/nrf52_to_nrf52810.h \
+ ../../../../../../modules/nrfx/mdk/nrf52810_to_nrf52811.h \
+ ../../../../../../modules/nrfx/mdk/compiler_abstraction.h \
+ C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 5.64/include/stdio.h \
+ C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 5.64/include/__crossworks.h \
+ C:\nordic\nRF5_SDK_17.1.0_ddde560\components\libraries\util\app_error.h \
+ C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 5.64/include/stdbool.h \
+ C:\nordic\nRF5_SDK_17.1.0_ddde560\components\libraries\util\sdk_errors.h \
+ ../../../../../../components/softdevice/s132/headers/nrf_error.h \
+ C:\nordic\nRF5_SDK_17.1.0_ddde560\components\libraries\util\nordic_common.h \
+ C:\nordic\nRF5_SDK_17.1.0_ddde560\components\libraries\util\app_error_weak.h
---
+++ pca10040/s132/ses/Output/Release/Obj/ble_app_beacon_pca10040_s132/thumb_crt0.d
@@ -0,0 +1,2 @@
+Output/Release/Obj/ble_app_beacon_pca10040_s132/thumb_crt0.o: \
+ C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 5.64/source/thumb_crt0.s
